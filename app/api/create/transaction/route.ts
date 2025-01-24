ac24303872327bf4b2af389d2bdcf533a3d269da
Al Aqmar <alaqmarak0810@gmail.com>
2025-01-24 17:48:09 +0530

    retry

--- app/api/create/transaction/route.ts
+++ app/api/create/transaction/route.ts
@@ -1,49 +1,58 @@
 import prismadb from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function POST(req:Request){
-    const data = await req.json();
-    const { towardsId, amount, message, comments, statusId, date, handlerId, paymentModeId } = data;
-    const month = new Date(date).getMonth() + 1;
-    const year = new Date(date).getFullYear();
+export async function POST(req: Request) {
+  const data = await req.json();
+  const {
+    towardsId,
+    amount,
+    message,
+    comments,
+    statusId,
+    date,
+    handlerId,
+    paymentModeId,
+  } = data;
+  const month = new Date(date).getMonth() + 1;
+  const year = new Date(date).getFullYear();
 
-    const monthYear = `${month}-${year}`;
+  const monthYear = `${month}-${year}`;
 
-    const monthYearData = await prismadb.monthYear.findFirst({
-        where: {
-            id: monthYear,
-        },
-    })
+  const monthYearData = await prismadb.monthYear.findFirst({
+    where: {
+      id: monthYear,
+    },
+  });
 
-    if(!monthYearData){
-        await prismadb.monthYear.create({
-            data: {
-                id: monthYear,
-                month,
-                year,
-            },
-        });
-    }
-    try{
-        const newTransaction = await prismadb.transaction.create({
-            data:{
-                towardsId,
-                amount,
-                message,
-                paymentModeId,
-                comments,
-                statusId,
-                date,
-                handlerId,
-                monthYearId: monthYear
-            }
-        });
-        return NextResponse.json({
-            status: 200,
-            message: "Transaction created successfully",
-            data: newTransaction,
-        });
-    }catch(error:any){
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+  if (!monthYearData) {
+    await prismadb.monthYear.create({
+      data: {
+        id: monthYear,
+        month,
+        year,
+      },
+    });
+  }
+  try {
+    const newTransaction = await prismadb.transaction.create({
+      data: {
+        towardsId,
+        amount,
+        message,
+        paymentModeId,
+        comments,
+        statusId,
+        date,
+        handlerId,
+        monthYearId: monthYear,
+      },
+    });
+    return NextResponse.json({
+      status: 200,
+      message: "Transaction created successfully",
+      data: newTransaction,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
