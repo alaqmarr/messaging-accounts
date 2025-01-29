97d2bde424aa7fa0d47f10928937e7ad2470802d
Al Aqmar <alaqmarak0810@gmail.com>
2025-01-29 15:22:16 +0530

    loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,9 +3,19 @@ import React from 'react'
 const loading = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <p>
-        Loading...
-      </p>
+      <div className="card">
+        <div className="loader">
+          <p>loading</p>
+          <div className="words">
+            <span className="word">data</span>
+            <span className="word">transactions</span>
+            <span className="word">messages</span>
+            <span className="word">cards</span>
+            <span className="word">buttons</span>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
